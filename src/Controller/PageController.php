634d657342ad4b84945b185498dee7e543a4114a
Israel Paz <israel_paz@msn.com>
2022-08-23 11:47:37 -0500

    Clase 12 Estructura inicial

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,17 +8,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PageController extends AbstractController
 {
     #[Route("/")]
-    public function home(Request $request) : Response
+    public function home(EntityManagerInterface $entityManager) : Response
     {
-        $search = $request->get('search');
-
-        return $this->render('home.html.twig', [
-            'search' => $search
+        $comments = $entityManager->getRepository(Comment::class)->findAll([], [
+            'id' => 'DESC'
+        ]);
+        return $this->render('home.html.twig',[
+            // 'comments' => $entityManager->getRepository(Comment::class)->findAll()
+            'comments' => $comments
         ]);
     }
 }
